996cbc48923b5e3d40ba1c4858c587e741b2b3b3
ramantayal12 <ramantayal12@gmail.com>
2021-02-22 17:21:32 +0530

    project working features 1. Can add new customers 2. view customers till now

--- src/main/java/customer_window.java
+++ src/main/java/customer_window.java
@@ -12,16 +12,20 @@ public class customer_window {
         JButton but1 = new JButton("Add new Customer");
         JButton but2 = new JButton("Show Existing Customers ");
 
-        but1.setBounds(200,100,200,20);
-        but2.setBounds(200,120,200,20);
-
+        but1.setBounds(200,100,500,50);
         jf.add(but1);
+
+        but2.setBounds(200,150,500,50);
         jf.add(but2);
 
         but1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                add_customer();
+                try {
+                    add_customer();
+                } catch (ClassNotFoundException | SQLException classNotFoundException) {
+                    classNotFoundException.printStackTrace();
+                }
             }
         });
         but2.addActionListener(new ActionListener() {
@@ -32,29 +36,12 @@ public class customer_window {
         });
 
         jf.setVisible(true);
-
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 
 
-    static void add_customer(){
-
-        Connection c = null;
-        try{
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:customer_data");
-            customer_window.customer();
-        }
-        catch( Exception e ){
-            wrong_login_window wrw = new wrong_login_window();
-            String text = e.getMessage();
-            String title = "database issue ";
-            wrw.show_window(title,text);
-        }
-    }
-
-     public static void customer(){
+     public static void add_customer() throws ClassNotFoundException, SQLException {
 
          JFrame jf = new JFrame("Add new Customer ");
          jf.setSize(800,800);
@@ -67,7 +54,27 @@ public class customer_window {
          JTextField bal = new JTextField();
          JTextField id = new JTextField();
 
-         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         JButton but1 = new JButton("Submit ");
+         but1.addActionListener(new ActionListener() {
+             @Override
+             public void actionPerformed(ActionEvent e) {
+                 try {
+                     Class.forName("org.sqlite.JDBC");
+                     Connection c = DriverManager.getConnection("jdbc:sqlite:customer_data");
+                     Statement statement = c.createStatement();
+                     ResultSet rs = statement.executeQuery("Insert into customer_data (User_ID, Name, Balance ) values (" + id + "," + name + "," + bal );
+                     rs.close();
+                     statement.close();
+                     c.close();
+                 }catch(Exception ex ){
+                     wrong_login_window wrw = new wrong_login_window();
+                     String text = ex.getMessage();
+                     String title = "database issue ";
+                     wrw.show_window(title,text);
+                 }
+             }
+         });
+
 
          customer_name.setBounds(200,100,200,50);
          name.setBounds(200,150,200,50);
@@ -75,7 +82,7 @@ public class customer_window {
          bal.setBounds(200,250,200,50);
          user_id.setBounds(200,300,200,50);
          id.setBounds(200,350,200,50);
-
+         but1.setBounds(200,400,100,50);
 
          jf.add(customer_name);
          jf.add(name);
@@ -83,16 +90,53 @@ public class customer_window {
          jf.add(bal);
          jf.add(user_id);
          jf.add(id);
+         jf.add(but1);
+
 
          jf.setVisible(true);
+         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 
     public static void show_customers(){
 
-        JFrame jf = new JFrame("Add new Customer ");
-        jf.setSize(800,800);
+        JFrame jf = new JFrame("List of Customers ");
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        jf.setSize(1000,1000);
+        try {
+            Class.forName("org.sqlite.JDBC");
+            Connection c = DriverManager.getConnection("jdbc:sqlite:customer_data");
+            Statement statement = c.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM customer_data");
+            int i = 0;
+            while (rs.next())
+            {
+                int id = rs.getInt("User_ID");
+                String name = rs.getString("Name");
+                int balance = rs.getInt("Balance");
+
+
+                JLabel label_id = new JLabel(String.valueOf(id));
+                JLabel label_name = new JLabel(String.valueOf(name));
+                JLabel label_bal = new JLabel(String.valueOf(balance));
+
+                label_id.setBounds( 10,(i+2)*100,100,50);
+                label_name.setBounds(150,(i+2)*100,100,50);
+                label_bal.setBounds(400,(i+2)*100,100,50);
+
+                jf.add(label_id);
+                jf.add(label_name);
+                jf.add(label_bal);
+                i += 1;
+            }
+            statement.close();
+        } catch (ClassNotFoundException | SQLException classNotFoundException) {
+            classNotFoundException.printStackTrace();
+        }
+
+        jf.setVisible(true);
 
     }
+
 }
--- src/main/java/main_class.java
+++ src/main/java/main_class.java
@@ -1,7 +1,6 @@
 public class main_class {
     public static void main(String []args ){
-        customer_window c = new customer_window();
         //login l = new login();
-        c.add_customer();
+        customer_window.show_options();
     }
 }
